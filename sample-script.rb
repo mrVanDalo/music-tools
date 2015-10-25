410fad1740615b9ed603d838cff814b51c59372e
Ingolf Wagner <palipalo9@googlemail.com>
2015-10-25 21:37:25 +0100

    now we can exclude tags from folders

--- sample-script.rb
+++ sample-script.rb
@@ -1,111 +1,3 @@
-
-$samples_dir = '/home/renoise/Samples'
-$target_dir = '/home/renoise/Renoise/User Library/Samples'
-
-# need to filter out files which are loops from drum/kick (for example)
-
-
-# tags which will not result in a folder
-$meta_tags = {
-    :tr808 => [ 'tr808' ],
-    :mc909 => [ 'mc-909', 'mc909' ],
-    :kick                => [ "kick", "bassdrum"],
-    :hihat               => [ "hihat", "hat", 'hit','openhi', 'closedhi'],
-    :clap                => [ "clap"],
-    :snare               => [ "snare"],
-    :ride                => [ "ride"],
-    :rim                 => [ 'rim'],
-    :crash               => [ "crash", "cymbal", 'cym'],
-    :tom                 => ['tom'],
-}
-
-# to filter out pbm
-$meta_tags_bpm = (90..300).map do |bpm|
-    { "#{bpm}bpm" =>
-      [
-          "#{bpm}bpm",
-          "#{bpm}-bpm",
-          "#{bpm}_bpm",
-          "#{bpm} bpm",
-          "#{bpm}.bpm",
-      ]
-    }
-end
-$meta_tags_bpm = $meta_tags_bpm.reduce( {}, :merge )
-$meta_folders_bpm = $meta_tags_bpm.keys.map do |tag|
-    [ :loop, tag ]
-end
-
-# tags which will result in a folder (:all)
-$folder_tags = {
-    :vinyl               => [ 'vinyl' ],
-    :pulse               => [ 'pulse' ],
-    :drumkit             => [ "drumkit", 'drum', 'kit'],
-    :percussion_acoustic => [ "clave", "tom", "bongo", "conga", "shaker", "mallet", "wood", "whistle", 'stick', 'cow'],
-    :percussion_mixed    => [ "percussion", "perc"],
-    :loop                => [ "loop" ],
-    :instrument          => [ "instrument"],
-    :keyboard            => [ "keyboard", "keys"],
-    :breakbeat           => [ "breakbeat"],
-    :guitar              => [ "guitar"],
-    :groove              => [ "groove", "groovy"],
-    :minimal             => [ "minimal", "glitch"],
-    :scratch             => [ "scratch", "turntabl"],
-    :reverse             => [ "reverse"],
-    :jazz                => [ "jazz", "swing"],
-    :brass               => [ "brass", "tuba"],
-    :rhodes              => [ "wurly", "wurlitz", "rhodes"],
-    :strings             => [ "string", "pizzi"],
-    :synth               => [ 'synth'],
-    :oneshots            => [ "oneshot", "shot"],
-    :pads                => [ "pad", 'atmo'],
-    :lead                => [ "lead"],
-    :bass                => [ "bass"],
-    :vocal               => [ "vocal", "vox", "female", "male", "human"],
-    :fx                  => [ "fx"],
-    :sweep               => [ "woosh", "sweep"],
-    :funkdisco           => [ "disco", "funk"],
-    :robotvocoder        => [ "vocoder", "robot"],
-    :piano               => [ 'piano', 'keyboard' ],
-    :organ               => [ 'organ' ],
-    :deep                => ['deep'],
-}
-
-# tags to extract from sample Map( TagName , List( StringMatches ) )
-$all_tags = $folder_tags.merge( $meta_tags ).merge( $meta_tags_bpm )
-
-# folders which will be created List( List( FolderName ))
-$folders = [
-    [ :loop    , :drumkit             ] ,
-    [ :loop    , :bass ] ,
-    [ :loop    , :kick ] ,
-    [ :loop    , :percussion_acoustic ] ,
-    [ :loop    , :percussion_mixed    ] ,
-    [ :drumkit , :kick                ] ,
-    [ :drumkit , :hihat               ] ,
-    [ :drumkit , :clap                ] ,
-    [ :drumkit , :snare               ] ,
-    [ :drumkit , :ride                ] ,
-    [ :drumkit , :rim                 ] ,
-    [ :drumkit , :crash               ] ,
-    [ :drumkit , :tom                 ] ,
-    [ :drumkit , :tr808               ] ,
-
-]
-$folders += $meta_folders_bpm
-$folders += $folder_tags.keys.map do |item|
-    [ item, :all ]
-end
-
-def folder_name( tags )
-    return File.join( tags.map { |tag| tag.to_s } )
-end
-
-def belongs_to_folder? ( tags , sample )
-    sample_tags = sample.tags + [:all]
-    return (tags - sample_tags).empty?
-end
-
 class Sample
 
     attr_accessor :path
@@ -148,6 +40,129 @@ class Sample
 
 end
 
+class Folder
+
+    attr_accessor :include
+    attr_accessor :exclude
+
+    # include : included tags
+    # exclude : excluded tags
+    def initialize( include_tags , exclude_tags = [] )
+        @include = include_tags
+        @exclude = exclude_tags
+    end
+
+    def name
+        return File.join( @include.map { |tag| tag.to_s } )
+    end
+
+    def belongs_to? ( sample )
+        sample_tags = sample.tags + [:all]
+        all_tags_included = ( @include - sample_tags).empty?
+        no_exclude_tag = ( sample_tags - @exclude ).size == sample_tags.size
+
+        return all_tags_included && no_exclude_tag
+    end
+
+end
+
+
+$samples_dir = '/home/renoise/Samples'
+$target_dir = '/home/renoise/Renoise/User Library/Samples'
+
+# need to filter out files which are loops from drum/kick (for example)
+
+# tags which will not result in a folder
+$meta_tags = {
+    :tr808 => [ 'tr808' ],
+    :mc909 => [ 'mc-909', 'mc909' ],
+    :kick  => [ "kick", "bassdrum"],
+    :hihat => [ "hihat", "hat", 'hit','openhi', 'closedhi'],
+    :clap  => [ "clap"],
+    :snare => [ "snare"],
+    :ride  => [ "ride"],
+    :rim   => [ 'rim'],
+    :crash => [ "crash", "cymbal", 'cym'],
+    :tom   => ['tom'],
+}
+
+# to filter out pbm
+$meta_tags_bpm = (90..200).map do |bpm|
+    { "#{bpm}bpm" =>
+      [
+          "#{bpm}bpm",
+          "#{bpm}-bpm",
+          "#{bpm}_bpm",
+          "#{bpm} bpm",
+          "#{bpm}.bpm",
+      ]
+    }
+end
+$meta_tags_bpm = $meta_tags_bpm.reduce( {}, :merge )
+$meta_folders_bpm = $meta_tags_bpm.keys.map do |tag|
+    Folder.new([ :loop, tag ])
+end
+
+# tags which will result in a folder (:all)
+$folder_tags = {
+    :vinyl               => [ 'vinyl' ],
+    :pulse               => [ 'pulse' ],
+    :drumkit             => [ "drumkit", 'drum', 'kit'],
+    :percussion_acoustic => [ "clave", "tom", "bongo", "conga", "shaker", "mallet", "wood", "whistle", 'stick', 'cow'],
+    :percussion_mixed    => [ "percussion", "perc"],
+    :loop                => [ "loop" ],
+    :instrument          => [ "instrument"],
+    :keyboard            => [ "keyboard", "keys"],
+    :breakbeat           => [ "breakbeat"],
+    :guitar              => [ "guitar"],
+    :groove              => [ "groove", "groovy"],
+    :minimal             => [ "minimal", "glitch"],
+    :scratch             => [ "scratch", "turntabl"],
+    :reverse             => [ "reverse"],
+    :jazz                => [ "jazz", "swing"],
+    :brass               => [ "brass", "tuba"],
+    :rhodes              => [ "wurly", "wurlitz", "rhodes"],
+    :strings             => [ "string", "pizzi"],
+    :synth               => [ 'synth'],
+    :oneshots            => [ "oneshot", "shot"],
+    :pads                => [ "pad", 'atmo'],
+    :lead                => [ "lead"],
+    :bass                => [ "bass"],
+    :vocal               => [ "vocal", "vox", "female", "male", "human"],
+    :fx                  => [ "fx"],
+    :sweep               => [ "woosh", "sweep"],
+    :funkdisco           => [ "disco", "funk"],
+    :robotvocoder        => [ "vocoder", "robot"],
+    :piano               => [ 'piano', 'keyboard' ],
+    :organ               => [ 'organ' ],
+    :deep                => ['deep'],
+}
+
+# tags to extract from sample Map( TagName , List( StringMatches ) )
+$all_tags = $folder_tags.merge( $meta_tags ).merge( $meta_tags_bpm )
+
+# folders which will be created List( List( FolderName ))
+$folders = [
+    Folder.new( [ :loop    , :drumkit ] ),
+    Folder.new( [ :loop    , :bass ] ),
+    Folder.new( [ :loop    , :kick ] ),
+    Folder.new( [ :loop    , :percussion_acoustic ] ),
+    Folder.new( [ :loop    , :percussion_mixed ] ),
+    Folder.new( [ :drumkit , :kick  ] , [ :loop ] ),
+    Folder.new( [ :drumkit , :hihat ] , [ :loop ] ),
+    Folder.new( [ :drumkit , :clap  ] , [ :loop ] ),
+    Folder.new( [ :drumkit , :snare ] , [ :loop ] ),
+    Folder.new( [ :drumkit , :ride  ] , [ :loop ] ),
+    Folder.new( [ :drumkit , :rim   ] , [ :loop ] ),
+    Folder.new( [ :drumkit , :crash ] , [ :loop ] ),
+    Folder.new( [ :drumkit , :tom   ] , [ :loop ] ),
+    Folder.new( [ :drumkit , :tr808 ] , [ :loop ] ),
+]
+$folders += $meta_folders_bpm
+$folders += $folder_tags.keys.map do |item|
+    Folder.new([ item, :all ])
+end
+
 
 def find_samples(samples_dir)
     samples = []
@@ -179,13 +194,13 @@ end
 
 $sample_collection = {}
 $folders.each do |folder|
-    $sample_collection[ folder_name( folder ) ] = []
+    $sample_collection[ folder.name ] = []
 end
 
 find_samples($samples_dir).each do |sample|
     $folders.each do |folder|
-        if (belongs_to_folder?( folder, sample))
-            $sample_collection[ folder_name ( folder ) ] += [sample]
+        if (folder.belongs_to?( sample))
+            $sample_collection[ folder.name ] += [sample]
         end
     end
 end
@@ -231,6 +246,3 @@ $sample_result.each do |sample|
     )
 end
 
-#p $samples
-
-#$folders.each { |folder| puts folder_name( folder ) }
